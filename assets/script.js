eca7f66320b007ee40b80aaf8d1133cfa89c39d4
Sean Oh <seanoh@ucsb.edu>
2022-06-29 21:12:35 -0700

    write pseudocode for js

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+// On page load
+$(function() {
+  // Selectors
+
+    // container selector
+    // current day selector
+
+  // Variables
+    // saved events 
+
+
+  // Functions
+
+    // load saved events
+
+    // display rows for each hour in the work day 
+      // For hours 9am to 5pm :
+        // create a row
+        // create hour column with hour of the day
+        // create a text area column
+          // color background based on current time
+          // fill in with saved text if it exists
+        // create a save event button
+        // append elements
+
+    // display current day
+
+    // save event
+
+
+  // Function Calls
+
+
+  // Event Listeners
+
+    // on container click, filter for save button and save event
+
+
+});
