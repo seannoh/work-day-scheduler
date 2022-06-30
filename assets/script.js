9e5c5337b8f728eb6c9bd5c391f8f106e6a00e64
Sean Oh <seanoh@ucsb.edu>
2022-06-29 21:26:53 -0700

    add selectors and load events

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,27 @@ $(function() {
   // Selectors
 
     // container selector
+    var containerEl = $(".container");
     // current day selector
+    var currentDayEl = $("#currentDay");
 
   // Variables
     // saved events 
+    var savedEvents = [
+      {text: ""}, {text: ""}, {text: ""}, {text: ""}, {text: ""}, {text: ""}, {text: ""}, {text: ""}, {text: ""}, 
+    ];
 
 
   // Functions
 
     // load saved events
+    function loadEvents() {
+      var rawEvents = localStorage.getItem("workday-events");
+      if(rawEvents){
+        var parsedEvents = JSON.parse(localStorage.getItem("workday-events"));
+        savedEvents = parsedEvents;
+      }
+    }
 
     // display rows for each hour in the work day 
       // For hours 9am to 5pm :
@@ -26,6 +38,12 @@ $(function() {
     // display current day
 
     // save event
+    function saveEvent(e) {
+      var text = $(this).parent().prev().val();
+      // get index of row (saved as a data attribute)
+      // set text of savedEvents at that index to text
+      // stringify and save
+    }
 
 
   // Function Calls
